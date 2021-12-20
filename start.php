aab235062c862b3af218f041e8cf53094017f937
blackbrokkoli <kastomat@gmail.com>
2021-12-20 12:58:51 +0100

    replace config values

--- start.php
+++ start.php
@@ -11,6 +11,6 @@ spl_autoload_register(function($class) {
 
 // Define erstellt eine Konstante, die kann dann im folgenden einfach mit dem namen verwendet werden... z.B. CHAT_SERVER_URL (ohne $)
 define("CHAT_SERVER_URL", "https://online-lectures-cs.thi.de/chat");
-define("CHAT_SERVER_ID", "186411aa-a8c2-4f94-bb55-0562d085d5f0");
+define("CHAT_SERVER_ID", "8595c8a0-6660-4086-9b2b-96af1b0e50d0");
 $service = new Utils\BackendService(CHAT_SERVER_URL, CHAT_SERVER_ID);
 ?>
